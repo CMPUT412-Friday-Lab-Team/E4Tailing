024b61294ad3185c3b6608d9d3607527ae748c93
KarlHanEdn <tianming@ualberta.ca>
2023-03-10 16:15:42 -0700

    test

--- python_files/lane_following/entry.py
+++ python_files/lane_following/entry.py
@@ -290,7 +290,7 @@ class LaneFollowingNode:
                 self.speed = self.max_speed
                 if turn_idx == 0:
                     print('making a left turn')
-                    self.controller.driveForTime(.7 * self.speed, 1.3 * self.speed, PROCESSING_RATE * 1.6)
+                    self.controller.driveForTime(.64 * self.speed, 1.36 * self.speed, PROCESSING_RATE * 1.8)
                 elif turn_idx == 1:
                     print('making a forward turn')
                     self.controller.driveForTime(1.1 * self.speed, .9 * self.speed, PROCESSING_RATE * 1.5)
