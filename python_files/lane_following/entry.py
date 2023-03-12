128827efd49bb6eb68adb168ce2627a3efa05cfc
KarlHanEdn <tianming@ualberta.ca>
2023-03-12 17:31:11 -0600

    test

--- python_files/lane_following/entry.py
+++ python_files/lane_following/entry.py
@@ -321,6 +321,12 @@ class LaneFollowingNode:
             contour_y = ymin + height * 0.5
 
         if self.turn_flag:
+            if publish_flag:
+                last_observed_x, last_observed_y = self.detection_manager.getCenter()
+                cv2.arrowedLine(im,
+                    (int(last_observed_x), int(last_observed_y)), 
+                    (int(last_observed_x), int(last_observed_y + 10)), 
+                    (255, 255, 0), 3)
             if self.detection_manager.isSafeToTurn():
                 if self.controller.actionQueueIsEmpty():
                     # make a turn
